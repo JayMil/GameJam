cf82993c256c42a3935b6d626a711258b1ace1db
JayMil <jmilder.lp@gmail.com>
2020-09-12 17:26:48 -0500

    Add race class and item class

---
+++ item.py
@@ -0,0 +1,10 @@
+
+class Item():
+    def __init__(self, parent):
+        self.x = parent.x
+        self.y = parent.y
+        self.moving = parent.moving
+        self.facing = parent.facing
+
+    def update(dt, moving, facing, xdiff, ydiff):
+        pass
