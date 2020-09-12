14d05c5d2608789b327e478e0411709fbcc79d6b
Aubrey Yates <aubreyyates@futurewebservice.com>
2020-09-12 16:05:38 -0500

    Add collision object

---
+++ CollisionObject.py
@@ -0,0 +1,29 @@
+class CollisionObject():
+    
+    ''' Generic collision object class '''
+    def __init__(self, start_pos, height, width, interaction):
+        self.x = start_pos[0]
+        self.y = start_pos[1]
+        self.height = height
+        self.width = width
+        self.interaction = interaction
+
+    def collides_with(self, other_object):
+        # rectangle collision
+        x1 = self.x
+        y1 = self.y
+        x2 = other_object.x
+        y2 = other_object.y
+
+        if (x1 < x2 + other_object.width and
+           x1 + self.width > x2 and
+           y1 < y2 + other_object.height and
+           y1 + self.height > y2):
+            return True
+        else:
+            return False
+
+class InteractionEnum(Enum):
+    BLOCKING = 1
+    MOVABLE = 2
+    NONE = 3
